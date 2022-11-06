d2a77d84d1a772a503a169f31f2e4eb71f2fd639
Michael Sharpe <m.sharpe@project89.org>
2022-11-05 18:42:52 -0700

    Start work on character describer

--- src/actors/makeNpc.ts
+++ src/actors/makeNpc.ts
@@ -1,21 +1,27 @@
-import * as ex from "excalibur";
+import { Actor, CollisionType, Color, Font, FontUnit, Label, vec, Vector } from "excalibur";
 import * as Components from "../ecs/components";
 import npcBT from "../behaviourTree/npc-bt";
 import { game } from "../game";
 
+interface NpcActor extends Actor {
+  metadata: Record<string, string>
+}
+
 //
 /////////// ENTITIES //////////////
 //
-export const makeNpc = (name, offset, needs) => {
-  const actor = new ex.Actor({
+export const makeNpc = async (name, offset, needs, _worldDescription) => {
+  const actor = new Actor({
     name: name,
-    pos: ex.vec(game.drawWidth / 2, 100).add(offset),
-    vel: ex.Vector.Zero,
+    pos: vec(game.drawWidth / 2, 100).add(offset),
+    vel: Vector.Zero,
     width: 30,
     height: 30,
-    color: ex.Color.Red,
-    collisionType: ex.CollisionType.Active
-  });
+    color: Color.Red,
+    collisionType: CollisionType.Active
+  }) as NpcActor
+
+  // const characterResponse = await callSpell('character-generator')
 
   actor
     .addComponent(new Components.NeedsComponent(needs))
@@ -26,7 +32,7 @@ export const makeNpc = (name, offset, needs) => {
   actor.on("precollision", function (ev) {
     // nudge to prevent "getting stuck"
     // TODO this doesn't work that great...
-    if (ev.target.vel.equals(ex.Vector.Zero))
+    if (ev.target.vel.equals(Vector.Zero))
       return;
 
     const nudge = ev.target.pos
@@ -37,10 +43,10 @@ export const makeNpc = (name, offset, needs) => {
     ev.target.pos.addEqual(nudge);
   });
 
-  const label = new ex.Label({
+  const label = new Label({
     text: name,
-    pos: ex.vec(-50, -30),
-    font: new ex.Font({ size: 16, unit: ex.FontUnit.Px })
+    pos: vec(-50, -30),
+    font: new Font({ size: 16, unit: FontUnit.Px })
   });
   actor.addChild(label);
 
