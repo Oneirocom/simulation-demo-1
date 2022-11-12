df35b9721721580e8e0c5b2128ff00c98bbd4149
Michael Sharpe <m.sharpe@project89.org>
2022-11-11 22:04:07 -0800

    Add more granular config options for simulating

--- src/config.ts
+++ src/config.ts
@@ -1,1 +1,9 @@
-export const simulate = true
+// override everything with the global simulate mocking
+export const simulate = true;
+
+// If simulate is false, these can be toggled to work on different pieces of the pipeline
+export const mockWorld = simulate || true;
+export const mockScene = simulate || true;
+export const mockObjects = simulate || false;
+export const mockCharacters = simulate || false;
+export const mockEnhanceWorldDescription = simulate || true;
