9c91a65f5c3bf9ef4c76325a27d6e57d9f55783c
Michael Sharpe <m.sharpe@project89.org>
2022-11-15 15:37:55 -0800

    Add generated scene and use it

--- src/main.ts
+++ src/main.ts
@@ -3,8 +3,10 @@ import { RandomScene } from "./scenes/randomScene";
 import * as Bridge from "./bridge";
 import { simulate } from "./config";
 import * as ArgosSDK from "./argos-sdk";
-import { generateDescriptions } from "./bridge";
+// import { generateDescriptions } from "./bridge";
 import "./style.css";
+import { GeneratedScene } from "./scenes/generatedScene";
+import { ArgosScene } from "./argos-sdk";
 
 // game.showDebug(true)
 if (simulate) console.debug("simulating ArgOS");
@@ -16,63 +18,81 @@ const beginButton = document.querySelector("#begin-game");
 const narrativeEl = document.querySelector("#narrative");
 const spinnerEl = document.querySelector("#spinner");
 
-const onReady = async (_worldDescription) => {
+/**
+ * Called after async actions to set up initial scene are done
+ */
+const onReady = async (argosScene: ArgosScene) => {
   loadingEls.forEach((el: HTMLElement) => el.classList.add("hidden"));
   statusEl.innerHTML = "Simulation running";
   describeButton.classList.remove("hidden");
   spinnerEl.classList.add("hidden");
 
-  // TODO could pass worldDescription or scene bluebrint in
-  const scene = new RandomScene();
+  // Passing in scene blueprint for the scene to be generated
+  const scene = new GeneratedScene(argosScene);
   console.log("running scene:", scene.name);
   game.add(scene.name, scene);
   game.goToScene(scene.name);
   game.start();
   // todo I dont like having to start the game here first, unless we want to show a loading screen until this is done.
 
-  const entities = (
-    game.currentScene as RandomScene
-  ).queries.describables.getEntities();
+  // const entities = (
+  //   game.currentScene as RandomScene
+  // ).queries.describables.getEntities();
 
-  const descriptionMap = await generateDescriptions(
-    _worldDescription,
-    entities
-  );
-  console.log("description map", descriptionMap);
+  // const descriptionMap = await generateDescriptions(
+  //   _worldDescription,
+  //   entities
+  // );
+  // console.log("description map", descriptionMap);
 };
 
+/**
+ * Starts the came when the begin simulation button is pressed
+ */
 beginButton.addEventListener("click", async (e) => {
   (e.target as HTMLButtonElement).disabled = true;
   spinnerEl.classList.remove("hidden");
 
   // TODO disable prompt inputs
-  const worldBody = {
+  const worldBodyinputs = {
     worldSummary:
       (document.querySelector("[name=world-summary]") as HTMLInputElement)
-        .value || "Everything is a simulatiuon",
+        .value || "The veil of unreality hides an unspeakable truth",
     genre:
       (document.querySelector("[name=genre]") as HTMLInputElement).value ||
-      "fantasy",
+      "science fiction",
     style:
       (document.querySelector("[name=style]") as HTMLInputElement).value ||
       "lovecraftian",
   };
 
+  const worldBody = {
+    ...worldBodyinputs,
+    numberOfObjects: 10,
+  };
+
   const worldResponse = await ArgosSDK.generateWorld(worldBody);
-  const worldDescription = worldResponse.outputs.worldDescription.trim();
+  const argosScene = worldResponse.outputs;
+  const worldDescription = argosScene.worldDescription.trim();
 
   addNarrative(worldDescription);
 
-  await onReady(worldDescription);
+  await onReady(argosScene);
 });
 
-function addNarrative(text: string) {
+/**
+ * Adds a bit of narrative output to the text rendering view
+ */
+export function addNarrative(text: string) {
   const narrativeItem = document.createElement("div");
   narrativeItem.className = "border border-black-500 rounded p-4 mb-4";
   narrativeItem.innerText = text;
   narrativeEl.appendChild(narrativeItem).scrollIntoView(true);
 }
 
+/**
+ * Pauses and plays the simulation.  On pause, sends a request to argos to narrate the story
+ */
 let isPaused = false;
 describeButton.addEventListener("click", (e) => {
   if (isPaused) {
