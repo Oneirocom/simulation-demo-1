e9d454063bc6f0e1e49263be2200ea0478604c2b
Michael Sharpe <m.sharpe@project89.org>
2022-11-15 15:48:25 -0800

    Fix build errors

--- src/scenes/mainScene.ts
+++ src/scenes/mainScene.ts
@@ -1,4 +1,4 @@
-import { Engine, Scene, SceneActivationContext, vec } from "excalibur";
+import { Scene, vec } from "excalibur";
 import Constants from "../constants";
 import * as Systems from "../ecs/systems";
 // import { game } from "../game";
@@ -6,8 +6,6 @@ import { makeNpc } from "../actors/makeNpc";
 import { forest } from "../actors/forest";
 import { field } from "../actors/field";
 import { firePit } from "../actors/firePit";
-import * as ArgosSDK from "../argos-sdk";
-
 
 // TODO use LifeTime component instead of settimeout
 const makeCampFire = () => {
@@ -21,40 +19,30 @@ const makeCampFire = () => {
  * Currently relied on by the BTs
  */
 const buildQueries = (world) => ({
-  heatSources: world.queryManager.createQuery([
-    Constants.HEATSOURCE
-  ]),
+  heatSources: world.queryManager.createQuery([Constants.HEATSOURCE]),
   combustibleResource: world.queryManager.createQuery([
-    Constants.COMBUSTIBLE_RESOURCE
+    Constants.COMBUSTIBLE_RESOURCE,
   ]),
-  edibleResource: world.queryManager.createQuery([
-    Constants.EDIBLE_RESOURCE
-  ])
-})
+  edibleResource: world.queryManager.createQuery([Constants.EDIBLE_RESOURCE]),
+});
 
 // TODO get these from a user prompt?  Or randomize?
-const worldSummary = 'Everything is a simulatiuon'
-const genre = 'fantasy'
-const style = 'lovecraftian'
-
-const worldBody = { worldSummary, genre, style }
-
 export class MainScene extends Scene {
-  worldDescription: string
-  sceneDescription: string
+  worldDescription: string;
+  sceneDescription: string;
   onReady: () => null;
 
   name = "mainScene";
 
   constructor(onReady) {
-    super()
+    super();
     this.onReady = onReady;
   }
-  
-  public async onInitialize(_engine: Engine): Promise<void> {
+
+  public async onInitialize(): Promise<void> {
     // todo show a loading icon here...
-      const worldResponse = await ArgosSDK.generateWorld(worldBody)
-      this.worldDescription = worldResponse.outputs.worldDescription
+    // const worldResponse = await ArgosSDK.generateWorld(worldBody)
+    // this.worldDescription = worldResponse.outputs.worldDescription
 
     // Not going to use the scene describer for now
     // const sceneBody = {
@@ -63,34 +51,38 @@ export class MainScene extends Scene {
 
     // const sceneResponse = await ArgosSDK.generateScene(sceneBody)
 
-    const queries = buildQueries(this.world)
+    const queries = buildQueries(this.world);
     this.world.add(new Systems.NeedsSystem());
     this.world.add(new Systems.CollectorSystem());
     this.world.add(new Systems.SeekSystem());
     // These get put into each bt blackboard
     // passing makeCampFire might be ugly
-    console.log("Initializing BT system", this)
+    console.log("Initializing BT system", this);
     this.world.add(new Systems.BTSystem({ makeCampFire, queries }));
 
     // TODO putting this at end of onActivate fires too soon, not sure this is the right lifecycle
     // also the initialize and activate events fire too soon too
-    this.onReady()
+    this.onReady();
   }
 
-  async onActivate(_context: SceneActivationContext<unknown>): Promise<void> {
+  async onActivate(): Promise<void> {
     // todo call the thoth spell here to generate a character.
     // keeping to one NPC for now.
-    const npc1 = await makeNpc("npc1", vec(-20, 20), { exposure: 5, hunger: 9 }, this.worldDescription)
+    const npc1 = await makeNpc(
+      "npc1",
+      vec(-20, 20),
+      { exposure: 5, hunger: 9 },
+      this.worldDescription
+    );
     // const npc2 = makeNpc("npc2", vec(20, 20), { exposure: 2, hunger: 2 });
     // const npc3 = makeNpc("npc3", vec(0, -20), { exposure: 0, hunger: 5 });
-  
+
     this.add(forest);
     this.add(field);
     this.add(firePit);
-  
+
     this.add(npc1);
     // this.add(npc2);
     // this.add(npc3);
-
   }
 }
