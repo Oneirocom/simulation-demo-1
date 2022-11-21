afa64633435783d986b96e08819c5794b72d474a
Michael Sharpe <m.sharpe@project89.org>
2022-11-21 09:00:51 -0800

    Switch order of or statements in config

--- src/config.ts
+++ src/config.ts
@@ -2,8 +2,11 @@
 export const simulate = true;
 
 // If simulate is false, these can be toggled to work on different pieces of the pipeline
-export const mockWorld = simulate || true;
-export const mockScene = simulate || true;
-export const mockObjects = simulate || false;
-export const mockCharacters = simulate || false;
-export const mockEnhanceWorldDescription = simulate || true;
+export const mockWorld = false || simulate;
+export const mockNarrative = false || simulate;
+
+// The below are mostly not used.  Audit these.
+export const mockScene = true || simulate;
+export const mockObjects = true || simulate;
+export const mockCharacters = true || simulate;
+export const mockEnhanceWorldDescription = true || simulate;
